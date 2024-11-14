e3d50fc550cf7596e5ebbc024a483828c5622803
phanhuyhiep <hiep2003dev@gmail.com>
2024-11-14 15:51:59 +0700

    edit base deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === 'production' ? '/frontend-client-mmaynenkhi/' : '/',
-  }
+export default defineConfig({
+  plugins: [react()],
+  base:"/frontend-client-mmaynenkhi/",
 })
